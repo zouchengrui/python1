7ac76b717fae68aac0acea0b00e3d67f7e7b82c9
zouchengrui <94594084@qq.com>
2017-03-12 15:03:14 +0800

    Add files via upload

---
+++ 0x14-000.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+#print(L[0][0])
+
+#print(L[1][1])
+
+print(L[2][2])
